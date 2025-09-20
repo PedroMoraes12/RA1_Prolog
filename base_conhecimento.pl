8ba475a9b3d5ab93e93ca9f89d4b70c955d4cb12
lucasferraz122 <lucasfersan10@gmail.com>
2025-09-20 19:44:56 -0300

    Update base_conhecimento.pl
    adicionado perfis de habilidade e pesos para todas as trilhas de peso 3 a 5 feito por: Lucas Ferraz

--- base_conhecimento.pl
+++ base_conhecimento.pl
@@ -5,3 +5,19 @@ trilha(inteligencia_artificial, 'Aprendizado de maquinas e raciocinio automatiza
 trilha(desenvolvimento_web,'Construcao de websites e aplicacoes web.').
 trilha(seguranca_da_informacao, 'Protecao de dados e seguranca de sistemas.').
 trilha(redes_e_infraestrutura, 'Administracao de redes e servidores.').
+
+perfil(ciencia_de_dados, matematica_estatistica, 5).
+perfil(ciencia_de_dados, programacao, 4).
+perfil(ciencia_de_dados, pensamento_logico, 3).
+perfil(inteligencia_artificial, programacao, 5).
+perfil(inteligencia_artificial, logica, 4).
+perfil(inteligencia_artificial, criatividade, 3).
+perfil(desenvolvimento_web, design_virtual, 5).
+perfil(desenvolvimento_web, programacao, 3).
+perfil(desenvolvimento_web, criatividade, 4).
+perfil(seguranca_da_informacao, analise_de_riscos, 5).
+perfil(seguranca_da_informacao, pensamento_logico, 4).
+perfil(seguranca_da_informacao, conhecimento_de_redes, 3).
+perfil(redes_e_infraestrutura, administracao_de_sistemas, 5).
+perfil(redes_e_infraestrutura, conhecimento_de_redes, 4).
+perfil(redes_e_infraestrutura, pensamento_logico, 3).
