eb0a049c9f9eeff3db969a01500703c40b1b474e
lucasferraz122 <lucasfersan10@gmail.com>
2025-09-20 18:54:56 -0300

    Update base_conhecimento.pl
    adicionar as 5 trilhas de especialização commit feito por Lucas Ferraz

--- base_conhecimento.pl
+++ base_conhecimento.pl
@@ -1,1 +1,7 @@
-#base conhecimento feito por: Lucas Ferraz
+#base de conhecimento feito por: Lucas Ferraz
+
+trilha(ciencia_de_dados, 'Analise de dados e estatistica aplicada.').
+trilha(inteligencia_artificial, 'Aprendizado de maquinas e raciocinio automatizado.').
+trilha(desenvolvimento_web,'Construcao de websites e aplicacoes web.').
+trilha(seguranca_da_informacao, 'Protecao de dados e seguranca de sistemas.').
+trilha(redes_e_infraestrutura, 'Administracao de redes e servidores.').
