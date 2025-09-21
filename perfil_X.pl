ab51f6116eace8087e74eb224394e74f2b20d9c3
PedroMoraes12 <pedrohenriquemoraes58@gmail.com>
2025-09-21 17:42:44 -0300

    Update perfil_X.pl
    adicionei teste para perfil design + criatividade

--- perfil_X.pl
+++ perfil_X.pl
@@ -16,3 +16,18 @@ teste_perfil_matematica_programacao :-
     assertz(resposta(9, n)), % Admin sistemas
     recomenda(Resultados),
     exibe_resultados(Resultados).
+
+teste_perfil_design_criatividade :-
+    write('=== PERFIL: Design + Criatividade ==='), nl,
+    retractall(resposta(_, _)),
+    assertz(resposta(1, n)), % Matemática
+    assertz(resposta(2, s)), % Programação
+    assertz(resposta(3, n)), % Pensamento lógico
+    assertz(resposta(4, n)), % Lógica
+    assertz(resposta(5, s)), % Criatividade
+    assertz(resposta(6, s)), % Design
+    assertz(resposta(7, n)), % Análise de riscos
+    assertz(resposta(8, n)), % Redes
+    assertz(resposta(9, n)), % Admin sistemas
+    recomenda(Resultados),
+    exibe_resultados(Resultados).
