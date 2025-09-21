35e1b3482f2a7b1600981190daf40ad817dad2b3
lipeerz <alanfilipe2006@gmail.com>
2025-09-21 18:33:51 -0300

    Update interface_usuario.pl

--- interface_usuario.pl
+++ interface_usuario.pl
@@ -3,7 +3,6 @@
 :- dynamic resposta/2
 
 Iniciar :-
-write('=========================================='), nl,
     write('   SISTEMA ESPECIALISTA - RECOMENDADOR DE TRILHAS'), nl,
     write('=========================================='), nl,
     write('Responda as perguntas abaixo com s (sim) ou n (nao).'), nl, nl,
