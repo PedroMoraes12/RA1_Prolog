d0d4fe32c8e78e2e4580209f2c9e3351c88485bd
lipeerz <alanfilipe2006@gmail.com>
2025-09-21 19:15:58 -0300

    Update interface_usuario.pl

--- interface_usuario.pl
+++ interface_usuario.pl
@@ -5,7 +5,7 @@
 iniciar :-
     write('SISTEMA ESPECIALISTA - RECOMENDADOR DE TRILHAS'), nl,
     write('Responda as perguntas abaixo com s (sim) ou n (nao).'), nl, nl,
-    retractall(resposta(_, _)), % limpa respostas anteriores
+    retractall(resposta(_, _)), 
     faz_perguntas,
     recomenda(Resultados),
     nl, write('===== RESULTADOS ====='), nl,
@@ -17,6 +17,6 @@ faz_perguntas :-
     read(Resp),
     (Resp == s ; Resp == n),      % só vai aceitar 's' ou 'n'    
     assertz(resposta(Id, Resp)),
-    fail.                            % força voltar e perguntar a próxima
-    faz_perguntas.                        % encerra quando não houver mais
+    fail.                            
+    faz_perguntas.                        
     
