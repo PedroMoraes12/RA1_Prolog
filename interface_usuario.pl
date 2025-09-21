7174663625c4dcca5de16cf62d00577fd94a1b27
lipeerz <alanfilipe2006@gmail.com>
2025-09-21 18:24:13 -0300

    Update interface_usuario.pl

--- interface_usuario.pl
+++ interface_usuario.pl
@@ -1,3 +1,9 @@
 %INTERFACE DE USUÁRIO - Alan Filipe
 
 :- dynamic resposta/2
+
+Iniciar :-
+write('=========================================='), nl,
+    write('   SISTEMA ESPECIALISTA - RECOMENDADOR DE TRILHAS'), nl,
+    write('=========================================='), nl,
+    write('Responda as perguntas abaixo com s (sim) ou n (nao).'), nl, nl,
