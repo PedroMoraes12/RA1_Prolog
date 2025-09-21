cdc9033c8191c6cc6c10a7a1bc9c6738ec94a3ae
lipeerz <alanfilipe2006@gmail.com>
2025-09-21 18:26:22 -0300

    Update interface_usuario.pl

--- interface_usuario.pl
+++ interface_usuario.pl
@@ -7,3 +7,8 @@ write('=========================================='), nl,
     write('   SISTEMA ESPECIALISTA - RECOMENDADOR DE TRILHAS'), nl,
     write('=========================================='), nl,
     write('Responda as perguntas abaixo com s (sim) ou n (nao).'), nl, nl,
+    retractall(resposta(_, _)), % limpa respostas anteriores
+    faz_perguntas,
+    recomenda(Resultados),
+    nl, write('===== RESULTADOS ====='), nl,
+    exibe_resultados(Resultados).
