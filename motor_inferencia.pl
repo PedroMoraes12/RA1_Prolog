3a484dd71f7eb490341815e1c530086c4d23595a
PedroMoraes12 <pedrohenriquemoraes58@gmail.com>
2025-09-22 19:06:20 -0300

    Update motor_inferencia.pl
    alteração na ordem

--- motor_inferencia.pl
+++ motor_inferencia.pl
@@ -1,8 +1,4 @@
-% MOTOR DE INFERÊNCIA - Pedro Henrique Moreaes
-
-calcula_pontuacao(Trilha, Pontuacao) :-
-    findall(Peso, (perfil(Trilha, Caracteristica, Peso), tem_caracteristica(Caracteristica)), Pesos),
-    soma_lista(Pesos, Pontuacao).
+% MOTOR DE INFERÊNCIA - Pedro Henrique Moraes
 
 tem_caracteristica(Caracteristica) :-
     pergunta(IdPergunta, _, Caracteristica),
@@ -13,24 +9,28 @@ soma_lista([H|T], Soma) :-
     soma_lista(T, SomaResto),
     Soma is H + SomaResto.
 
-recomenda(ResultadosOrdenados) :-
-    findall(Trilha, trilha(Trilha, _), Trilhas),
-    calcula_todas_pontuacoes(Trilhas, Resultados),
-    ordena_resultados(Resultados, ResultadosOrdenados).
+calcula_pontuacao(Trilha, Pontuacao) :-
+    findall(Peso, (perfil(Trilha, Caracteristica, Peso), tem_caracteristica(Caracteristica)), Pesos),
+    soma_lista(Pesos, Pontuacao).
 
 calcula_todas_pontuacoes([], []).
-calcula_todas_pontuacoes([Trilha|Resto], [Pontuacao-Trilha|Resultados]) :-
+calcula_todas_pontuacoes([Trilha|Resto], [Pontuacao-Trilha|Resultado]) :-
     calcula_pontuacao(Trilha, Pontuacao),
-    calcula_todas_pontuacoes(Resto, Resultados).
+    calcula_todas_pontuacoes(Resto, Resultado).
+
+inverter_pares([], []).
+inverter_pares([Pontuacao-Trilha|Resto], [Trilha-Pontuacao|Invertido]) :-
+    inverter_pares(Resto, Invertido).
 
 ordena_resultados(Resultados, Ordenados) :-
     inverter_pares(Resultados, Invertidos),
     keysort(Invertidos, OrdenadosInvertidos),
     inverter_pares(OrdenadosInvertidos, Ordenados).
 
-inverter_pares([], []).
-inverter_pares([Pontuacao-Trilha|Resto], [Trilha-Pontuacao|Invertidos]) :-
-    inverter_pares(Resto, Invertidos).
+recomenda(ResultadosOrdenados) :-
+    findall(Trilha, trilha(Trilha, _), Trilhas),
+    calcula_todas_pontuacoes(Trilhas, Resultados),
+    ordena_resultados(Resultados, ResultadosOrdenados).
 
 exibe_resultados([]).
 exibe_resultados([Pontuacao-Trilha|Resto]) :-
