c2e33910e3d2d8131f2b0b01f1c513e03e21a5d3
lucasferraz122 <lucasfersan10@gmail.com>
2025-09-22 01:01:49 -0300

    Update base_conhecimento.pl
    Adicionei mais perfis com peso 1 e 2 pra cada uma das trilhas feirto por: Lucas Ferraz

--- base_conhecimento.pl
+++ base_conhecimento.pl
@@ -9,18 +9,28 @@ trilha(redes_e_infraestrutura, 'Administracao de redes e servidores.').
 perfil(ciencia_de_dados, matematica_estatistica, 5).
 perfil(ciencia_de_dados, programacao, 4).
 perfil(ciencia_de_dados, pensamento_logico, 3).
+perfil(ciencia_de_dados, logica, 2).
+perfil(ciencia_de_dados, design_virtual, 1).
 perfil(inteligencia_artificial, programacao, 5).
 perfil(inteligencia_artificial, logica, 4).
 perfil(inteligencia_artificial, criatividade, 3).
+perfil(inteligencia_artificial, matematica_estatistica, 2).
+perfil(inteligencia_artificial, design_virtual, 1).
 perfil(desenvolvimento_web, design_virtual, 5).
 perfil(desenvolvimento_web, programacao, 3).
 perfil(desenvolvimento_web, criatividade, 4).
+perfil(desenvolvimento_web, logica, 2).
+perfil(desenvolvimento_web, matematica_estatistica, 1).
 perfil(seguranca_da_informacao, analise_de_riscos, 5).
 perfil(seguranca_da_informacao, pensamento_logico, 4).
 perfil(seguranca_da_informacao, conhecimento_de_redes, 3).
+perfil(seguranca_da_informacao, programacao, 2).
+perfil(seguranca_da_informacao, logica, 1).
 perfil(redes_e_infraestrutura, administracao_de_sistemas, 5).
 perfil(redes_e_infraestrutura, conhecimento_de_redes, 4).
 perfil(redes_e_infraestrutura, pensamento_logico, 3).
+perfil(redes_e_infraestrutura, analise_de_riscos, 2).
+perfil(redes_e_infraestrutura, programacao, 1).
 
 pergunta(1, 'Se da bem com matemática e/ou estatística?', matematica_estatistica).
 pergunta(2, 'Gosta de programar em alguma linguagem e possui pensamento crítico?', programacao).
