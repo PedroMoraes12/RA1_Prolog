55bd930f424e93467230803a4511f63b7734910f
PedroMoraes12 <pedrohenriquemoraes58@gmail.com>
2025-09-22 15:36:05 -0300

    Create perfil_123.pl

---
+++ perfil_123.pl
@@ -0,0 +1,6 @@
+% TESTE DOS 3 PERFIS DE USUARIO - Pedro Henrique Moreaes
+
+testa_tudo :-
+    teste_perfil_matematica_programacao, nl,
+    teste_perfil_design_criatividade, nl,
+    teste_perfil_redes_infra.
